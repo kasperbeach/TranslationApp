2d07a5d0976c7f83ef2c44ed59022f5dd0241daa
Jonathan Chambers <jonathan.chambers@selu.edu>
2018-06-20 16:22:59 -0500

    eased future implementation and current testing
    put the translate action into a function so it can be called, and added a small console app to see the text to text in action

--- Application/index.js
+++ Application/index.js
@@ -1,10 +1,23 @@
-const translate = require('google-translate-api');
+const translate = require('google-translate-api')
+const prompt = require('prompt')
 
-translate('Ich arbeite gerade', {to: 'en'}).then(res => {
-    console.log(res.text);
-    //=> I speak English
-    console.log(res.from.language.iso);
-    //=> nl
-}).catch(err => {
-    console.error(err);
-});
+
+consoleApp()
+
+function consoleApp(){
+    prompt.start()
+    prompt.get(['words','language'],function(err,result){
+        textToText(result.words, result.language)
+    })
+}
+
+function textToText(translateMe, lang){
+    translate(translateMe, {to: lang}).then(res => {
+        console.log(res.text)
+        //=> I speak English
+        console.log(res.from.language.iso)
+        //=> nl
+    }).catch(err => {
+        console.error(err)
+    })
+}
